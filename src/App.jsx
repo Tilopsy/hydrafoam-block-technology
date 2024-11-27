bd1e93f5412958011bda64c5e4f6f0b92d34bf7b
Your Name <bettylizzie25@gmail.com>
2024-11-27 11:26:43 +0000

    created hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
-// import LandingPage from './pages/landing-page';
+import LandingPage from './landing-page';
 import RootLayout from './layouts/rootLayout';
 import About from './landing-page/about';
 import { ToastContainer } from "react-toastify";
@@ -9,6 +9,8 @@ import Contact from './landing-page/contact';
 import Hero from './landing-page/hero';
 import Services from './landing-page/services';
 import Testimonials from './landing-page';
+import Blog from './landing-page';
+
 
 
 function App() {
@@ -40,6 +42,10 @@ function App() {
         {
           path: '/testimonials',
           element: <Testimonials />,
+        },
+        {
+          path: '/blog',
+          element: <Blog />,
         },
       ],
     },
